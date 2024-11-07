c3a25182413391642445197ecd8f1060abef01b0
Lean <leandroEzequiel2014@gmail.com>
2024-11-07 16:19:22 -0300

    Primer commit

---
+++ src/main/java/com/rappi/rappi/RappiApplication.java
@@ -0,0 +1,13 @@
+package com.rappi.rappi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RappiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RappiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rappi/rappi/RappiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rappi.rappi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RappiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
